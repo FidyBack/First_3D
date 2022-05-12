5c76b01ad75ac4148c889b3ee5ee26d3bedf7bdc
FidyBack <abel.cavalcante.ismart@gmail.com>
2022-05-12 16:48:27 -0300

    Movimento

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -7,36 +7,57 @@ public class PlayerController : MonoBehaviour {
 	CharacterController characterController;
 	GameObject playerCamera;
 
-	float _baseSpeed = 10.0f;
-	float _gravidade = 9.8f;
-	float cameraRotation;
+	float _mouseSensitivity = 125.0f;
+	float cameraRotation = 0.0f;
+
+	float _baseSpeed = 12.0f;
+	float _baseJumpHeight = 103.0f;
+	float _gravity = 9.8f;
 
 
 	void Start() {
 		characterController = GetComponent<CharacterController>();
 		playerCamera = GameObject.Find("Main Camera");
-		cameraRotation = 0.0f;
+		Cursor.lockState = CursorLockMode.Locked;
 	}
 
 	void Update() {
 		
-		float x = Input.GetAxis("Horizontal");
-		float z = Input.GetAxis("Vertical");
-		float mouse_dX = Input.GetAxis("Mouse X");
-		float mouse_dY = Input.GetAxis("Mouse Y");
+		if (!PauseMenu.GameIsPaused) {
+			Cursor.lockState = CursorLockMode.Locked;
+			// Rotation
+			float mouse_dX = Input.GetAxis("Mouse X") * _mouseSensitivity * Time.deltaTime;
+			float mouse_dY = Input.GetAxis("Mouse Y") * _mouseSensitivity * Time.deltaTime;
 
-		float y = 0;
-		if(!characterController.isGrounded){
-			y = -_gravidade;
+			cameraRotation -= mouse_dY;
+			cameraRotation = Mathf.Clamp(cameraRotation, -90, 90);
+			playerCamera.transform.localRotation = Quaternion.Euler(cameraRotation, 0.0f, 0.0f);
+			transform.Rotate(Vector3.up * mouse_dX);
+
+			// Movement
+			float x = Input.GetAxis("Horizontal");
+			float z = Input.GetAxis("Vertical");
+
+			float y = 0;
+			if(!characterController.isGrounded){
+				y -= _gravity;
+			}
+
+			Vector3 direction = transform.right * x + transform.up * y + transform.forward * z;
+			characterController.Move(direction * _baseSpeed * Time.deltaTime);
+
+			// Jump
+			if (Input.GetButtonDown("Jump") && characterController.isGrounded) {
+				y = _baseJumpHeight;
+			}
+		} 
+		else {
+			Cursor.lockState = CursorLockMode.None;
 		}
+	
 
-		Vector3 direction = transform.right * x + transform.up * y + transform.forward * z;
-		cameraRotation -= mouse_dY;
-		Mathf.Clamp(cameraRotation, -75.0f, 75.0f);
 
-		characterController.Move(direction * _baseSpeed * Time.deltaTime);
-		transform.Rotate(Vector3.up, 2*mouse_dX);
-		playerCamera.transform.localRotation = Quaternion.Euler(2*cameraRotation, 0.0f, 0.0f);
+
    }
 
 
@@ -45,7 +66,7 @@ public class PlayerController : MonoBehaviour {
 		Debug.DrawRay(playerCamera.transform.position, transform.forward*10.0f, Color.magenta);
 	
 		if(Physics.Raycast(playerCamera.transform.position, transform.forward, out hit, 100.0f)) {
-			Debug.Log(hit.collider.name);
+			// Debug.Log(hit.collider.name);
 		}
 	}
 }
